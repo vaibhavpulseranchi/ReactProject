e72bd1dd95b4f947ae18dee5afaf988349b4d921
Vaibhav Singh <vaibhav.singh@PS97.Team>
2022-09-20 09:14:39 +0530

    1st

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NameTag from './Components/NameTag';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+        <h1 style={{textAlign:'center'}}>Hello</h1>
+        <NameTag/>
+        </div>
   );
 }
 
